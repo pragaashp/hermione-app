71299c418e3f8a77479f55dad32fe6e0337470f1
ycloo <yc.lu@berkeley.edu>
2016-04-14 03:15:56 +0000

    done search boxes

--- app/controllers/ets_controller.rb
+++ app/controllers/ets_controller.rb
@@ -4,7 +4,14 @@ class EtsController < ApplicationController # TEMPORARY: AFTER CAS -> CHANGE TO 
   # before_action :is_ets?
 
   def index
-
+    if params[:room] and params[:building]
+      @courses = Course.search(params[:room] + " " + params[:building],params[:time])
+    elsif params[:room] 
+      @courses = Course.search(params[:room],params[:time])
+    elsif params[:building]
+      @courses = Course.search(params[:building],params[:time])
+    else
+      @courses = Course.all
+    end
   end
-
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -3,5 +3,8 @@ class Course < ActiveRecord::Base
   has_and_belongs_to_many :professors, join_table: :assignments
   has_one :request
 
-
+  def self.search(location,time)
+    Course.where(:location => location,:time =>time) 
+  end
+  
 end
